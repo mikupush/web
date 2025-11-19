63dc2d3200ffa498b703c0b5431560b1b06352e9
MagonxESP <janma.360@gmail.com>
2025-11-20 00:38:05 +0100

    chore: improve eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,18 @@
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 import eslintPluginAstro from "eslint-plugin-astro";
+import reactPlugin from 'eslint-plugin-react';
 
-/** @type {import("eslint").Linter.FlatConfig[]} */
-export default [
-  ...eslintPluginAstro.configs.recommended,
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
   {
     rules: {
       "astro/no-set-html-directive": "error"
     },
   },
-];
+)
