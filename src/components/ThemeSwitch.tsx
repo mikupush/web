022a7d423aed8c7e301ecf2cd501f811c25b616d
MagonxESP <janma.360@gmail.com>
2025-12-30 21:31:29 +0100

    feat: theme switch

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,11 +1,20 @@
 import {Button} from "@/components/ui/button.tsx";
+import {useState} from "react";
+import {DARK_THEME, getCurrentTheme, toggleDarkTheme} from "@/lib/theme.ts";
+import {Sun, Moon} from "lucide-react";
+import {useTranslation} from "react-i18next";
 
 export default function ThemeSwitch() {
-
+  const { t } = useTranslation();
+  const [theme, setTheme] = useState(getCurrentTheme());
 
   return (
-    <Button variant="ghost">
-
+    <Button
+      variant="ghost"
+      onClick={() => setTheme(toggleDarkTheme())}
+      aria-label={theme === DARK_THEME ? t('set_light_theme') : t('set_dark_theme')}
+    >
+      {theme === DARK_THEME ? <Sun /> : <Moon />}
     </Button>
   )
 }
--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,3 +1,30 @@
-export function toggleDarkTheme() {
+export const DARK_THEME = 'dark'
+export const LIGHT_THEME = 'light'
+export const THEME_KEY = 'theme'
 
+export function getCurrentTheme() {
+  const detectSystemTheme = () => {
+    const query = window.matchMedia('(prefers-color-scheme: dark)')
+    return query.matches ? DARK_THEME : LIGHT_THEME
+  }
+
+  return localStorage.getItem(THEME_KEY) ?? detectSystemTheme()
+}
+
+export function toggleDarkTheme() {
+  const currentTheme = getCurrentTheme()
+  const theme = currentTheme === DARK_THEME ? LIGHT_THEME : DARK_THEME
+
+  applyTheme(theme)
+  return theme
+}
+
+export function applyTheme(theme: string) {
+  if (theme === DARK_THEME) {
+    document.documentElement.classList.toggle('dark', true)
+  } else {
+    document.documentElement.classList.toggle('dark', false)
+  }
+
+  localStorage.setItem(THEME_KEY, theme)
 }
